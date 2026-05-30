1dd02819ca337872bda8ea747750f7f883aee70d
sangeethasanthosh <sangeetha90naik@gmail.com>
2026-05-30 09:14:12 +0530

    Actuator, Circuitbreaker added

--- src/main/java/com/microservice/patters/vaccinationcentre/VaccinationCentreApplication.java
+++ src/main/java/com/microservice/patters/vaccinationcentre/VaccinationCentreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+
 public class VaccinationCentreApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/microservice/patters/vaccinationcentre/controller/VaccinationController.java
+++ src/main/java/com/microservice/patters/vaccinationcentre/controller/VaccinationController.java
@@ -4,6 +4,7 @@ package com.microservice.patters.vaccinationcentre.controller;
 import com.microservice.patters.vaccinationcentre.entity.AggregateResponse;
 import com.microservice.patters.vaccinationcentre.entity.VaccinationCentre;
 import com.microservice.patters.vaccinationcentre.service.VaccionationService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,16 +31,26 @@ public class VaccinationController {
     }
 
     @GetMapping("/id/{id}")
+    @CircuitBreaker(name="CITIZEN_SERVICE",fallbackMethod="fetchDefault")
     public ResponseEntity<AggregateResponse> getCompleteDetails(@PathVariable Integer id){
         AggregateResponse aggregateResponse = new AggregateResponse();
 
         aggregateResponse.setVaccinationCentre(vaccionationService.getVaccinationCentreDetails(id));
        if(aggregateResponse.getVaccinationCentre()!=null) {
+           System.out.println("Calling Citizen Service");
            //List citizenList = restTemplate.getForObject("http://CITIZEN_SERVICE/citizen/id/" + id, List.class);
            List citizenList = restTemplate.getForObject("http://localhost:8081/citizen/id/" + id, List.class);
            aggregateResponse.setCitizens(citizenList);
        }
 
             return new ResponseEntity<>(aggregateResponse, HttpStatus.OK);
+    }
+
+    public ResponseEntity<AggregateResponse> fetchDefault(@PathVariable Integer id,Exception exception){
+       System.out.println("CitizenServiceDown");
+        AggregateResponse aggregateResponse = new AggregateResponse();
+
+        aggregateResponse.setVaccinationCentre(vaccionationService.getVaccinationCentreDetails(id));
+        return new ResponseEntity<>(aggregateResponse, HttpStatus.OK);
     }
 }
